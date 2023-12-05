f38ffada8e95fffbeaaf788ac41d7541af2ef0cf
Oybek <toraqulovoybek2@gmail.com>
2023-12-05 12:13:00 +0500

    first commit

--- lib/presentation/view/pages/home/home_page.dart
+++ lib/presentation/view/pages/home/home_page.dart
@@ -21,7 +21,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
+      
       body: Column(
         children: [
           Padding(
